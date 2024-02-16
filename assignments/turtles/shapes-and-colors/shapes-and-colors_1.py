12c171ceb10eb8d1aeaf93e54dff4dd4322b33f3
Eric Busboom <eric@civicknowledge.com>
2024-02-15 16:48:30 -0800

    .

--- assignments/turtles/shapes-and-colors/shapes-and-colors_1.py
+++ assignments/turtles/shapes-and-colors/shapes-and-colors_1.py
@@ -4,21 +4,25 @@ import turtle
 window = turtle.Screen()
 window.bgcolor('white')
 
-# This code makes a new Turtle. Pick a new name for the turtle
+# This code makes a new Turtle. Pick a new 
+# name for the turtle
 my_turtle = turtle.Turtle()
 
 # Make your turtle's shape 'turtle', .shape('turtle')
 
 # Set your turtle's speed using .speed(2)
 
-# Set your turtle's color using .color('green') and .pencolor('blue')
+# Set your turtle's color using .color('green') 
+# and .pencolor('blue')
 
 # Move your turtle forward using .forward(100)
 # TEST    Did your turtle move forward?
 
-# Move your turtle left or right using .left(90) or .right(90)
+# Move your turtle left or right using .left(90) 
+# or .right(90)
 
-# Now put the forward and left/right code into a for loop to repeat 4 times.
+# Now put the forward and left/right code into a for 
+# loop to repeat 4 times.
 # TEST    Did your turtle draw a square?
 
 # Move your turtle to a new place on the screen using .goto(x, y)
@@ -27,7 +31,8 @@ my_turtle = turtle.Turtle()
 # Have your turtle draw a circle using .circle(radius, steps=30)
 # TEST    Did your turtle draw a circle?
 
-# Add color to your shape by adding .begin_fill() before drawing the circle
+# Add color to your shape by adding .begin_fill() 
+# before drawing the circle
 # and .end_fill() below
 
 # Draw 3 more shapes with different fill colors!
